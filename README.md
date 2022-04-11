9b39ed1b247536cca38473f9d122680752bda531
TibiAlex <tibi.alex@yahoo.com>
2022-04-11 18:45:57 +0300

    Versiunea finala a temei

--- README.md
+++ README.md
@@ -1,50 +1,47 @@
 Nume: Buzera Tiberiu
 Grupă: 333CA
 
-# Tema <NR> TODO
-#### Este recomandat să folosiți diacritice. Se poate opta și pentru realizarea în limba engleză. 
+# Tema <NR> Marketplace
 
 Organizare
 -
-1. Explicație pentru soluția aleasă:
 
-***Obligatoriu:*** 
+Pentru implementarea temei am creat 2 dictionare, unul pentru retinerea producatorior si unul pentru
+retinerea produselor din cart.
+Fiecare cart are un id, si o lista asociata, fiecare element din lista este o pereche de id_producator
+si produs.
+Fiecare producator are un id si o lista cu produsele sale, fiecare element din lista este o pereche de 
+produs si o valoare de 1 sau 0, 1 inseamna ca acel produs este pe raft si 0 inseamna ca acel produs este in
+cos, acesta valoare are rolul de a nu modifica dimensiunea listei de produse pana nu se apeleaza place_order.
 
+In clasa consumer in metoda run se carcurge lista de comenzi primita in constructor si se apeleaza 
+metodele add sau remove din marketplace in functie de caz. In cazul in care produsul nu se gasea pe 
+raft se astepta o perioada de timp inainte sa se reincerce metoda de add.
 
-* De făcut referință la abordarea generală menționată în paragraful de mai sus. Aici se pot băga bucăți de cod/funcții - etc.
-* Consideri că tema este utilă?
-* Consideri implementarea naivă, eficientă, se putea mai bine?
+In clasa produser am implementat un while din care nu se iese pentru a se repeta la nesfarsit procesul de
+productie. In interiorul while-ului am parcurs lista de produse care aveau un timp de produse fiecare.
+Dupa ce se crea produsul se incerca sa se publice, in cazul in care coada din marketplace era plina se
+astepta un timp si se incerca publicarea produsului din nou.
 
-***Opțional:***
-
-
-* De menționat cazuri speciale, nespecificate în enunț și cum au fost tratate.
-
+Pentru lucrul cu threaduri am implementat 2 lock-uri unul pentru producatori si unul pentru consumatori
+si am delimitat in fiecare functie sectiunile critice.
 
 Implementare
 -
 
-* De specificat dacă întregul enunț al temei e implementat
-* Dacă există funcționalități extra, pe lângă cele din enunț - descriere succintă + motivarea lor
-* De specificat funcționalitățile lipsă din enunț (dacă există) și menționat dacă testele reflectă sau nu acest lucru
-* Dificultăți întâmpinate
-* Lucruri interesante descoperite pe parcurs
+Intregul enunt al temei a fost implementat.
+Am implementat in plus partea de logging prin care se afisaza informatii si erorile intalnite intr-un
+fisier numit marketplace.log.
+Am intampinat dificultati la implementarea temei in mod paralel, intrucat am avut probleme cu deadlock-urile.
+Pe parcursul temei am descoperit metoda de logging ca o buna metoda de debugging.
 
 
 Resurse utilizate
 -
 
-* Resurse utilizate - toate resursele publice de pe internet/cărți/code snippets, chiar dacă sunt laboratoare de ASC
+Ca resurse utilizate am folosit laboratoarele 1 (despre liste si dictionare) si laboratorul 2 (despre lock)
+petnru implementarea temei, plus link-urile puse la dispozitie in tema pentru implementarea logger-ului.
 
 Git
 -
-1. Link către repo-ul de git
-
-Ce să **NU**
--
-* Detalii de implementare despre fiecare funcție/fișier în parte
-* Fraze lungi care să ocolească subiectul în cauză
-* Răspunsuri și idei neargumentate
-* Comentarii (din cod) și *TODO*-uri
-
-Acest model de README a fost adaptat după [exemplul de README de la SO](https://github.com/systems-cs-pub-ro/so/blob/master/assignments/README.example.md).
+https://github.com/TibiAlex/MarketPlace
--- marketplace.py
+++ marketplace.py
@@ -6,6 +6,13 @@ Assignment 1
 March 2021
 """
 import threading
+import logging
+import logging.handlers
+
+# se creaza fisierul de log si se configureaza la nivelul info si in modul de write
+logging.basicConfig(filename='marketplace.log', level=logging.INFO)
+LOG = logging.handlers.RotatingFileHandler(filename='marketplace.log', mode='w',
+                                           maxBytes=1000, backupCount=0)
 
 
 class Marketplace:
@@ -21,23 +28,30 @@ class Marketplace:
         :type queue_size_per_producer: Int
         :param queue_size_per_producer: the maximum size of a queue associated with each producer
         """
-        self.queue_size_per_producer = queue_size_per_producer
-        self.lock_p = threading.Lock()  # lock :P
-        self.lock_c = threading.Lock()
+        logging.info("enter constructor")
 
-        self.cart_number = -1
-        self.cart_dex = {}
-        self.producer_number = -1
-        self.producer_dex = {}
+        self.queue_size_per_producer = queue_size_per_producer
+        self.lock_p = threading.Lock()  # lock pentru producator
+        self.lock_c = threading.Lock()  # lock pentru consumator
+
+        self.cart_number = -1  # numarul de carturi
+        self.cart_dex = {}  # dictionar de liste pentru a retine toate carturile
+        self.producer_number = -1   # numarul de producatori
+        self.producer_dex = {}  # dex de liste pentru a retine toti producatorii cu produsele lor
+
+        logging.info("leave constructor")
 
     def register_producer(self):
         """
         Returns an id for the producer that calls this.
+        Generate the id of the producer from the number of registrations until now
+        And add an empty list in the dex on the place where the key is the producer id
         """
         with self.lock_p:
+            logging.info("register producer")
             self.producer_number += 1
             self.producer_dex[self.producer_number] = []
-
+            logging.info("finished registering producer")
         return self.producer_number
 
     def publish(self, producer_id, product):
@@ -51,10 +65,19 @@ class Marketplace:
         :param product: the Product that will be published in the Marketplace
 
         :returns True or False. If the caller receives False, it should wait and then try again.
+
+        we verify if the queue is not full and add a product on the list in the dex
         """
-        if self.queue_size_per_producer < len(self.producer_dex[producer_id]):
-            return False
-        self.producer_dex[producer_id].append([product, 1])
+        with self.lock_p:
+            logging.info('try publishing %s on producer with id %s',
+                         product, producer_id)
+            if self.queue_size_per_producer < len(self.producer_dex[producer_id]):
+                logging.error('could not publish %s on producer with id %s',
+                              product, producer_id)
+                return False
+            self.producer_dex[producer_id].append([product, 1])
+            logging.info('finished publishing %s on producer with id %s',
+                         product, producer_id)
         return True
 
     def new_cart(self):
@@ -62,11 +85,16 @@ class Marketplace:
         Creates a new cart for the consumer
 
         :returns an int representing the cart_id
-        """
-        with self.lock_c:
-            self.cart_number += 1
-        self.cart_dex[self.cart_number] = []
 
+        Generate the id of the cart out of the number sof carts
+        Add an empty list in the dex
+        """
+
+        with self.lock_c:
+            logging.info("register new cart")
+            self.cart_number += 1
+            self.cart_dex[self.cart_number] = []
+            logging.info("finished registering new cart")
         return self.cart_number
 
     def add_to_cart(self, cart_id, product):
@@ -80,14 +108,20 @@ class Marketplace:
         :param product: the product to add to cart
 
         :returns True or False. If the caller receives False, it should wait and then try again
+
+        We verify if there is the product on the shelf, if it is we put it in the cart
+        and change the value of the product on the shelf from 1 to 0
         """
         with self.lock_p:
+            logging.info('try adding %s to cart with id %s', product, cart_id)
             for producer, _ in self.producer_dex.items():
                 for i, pair in enumerate(self.producer_dex[producer]):
                     if product == pair[0] and pair[1] == 1:
                         self.cart_dex[cart_id].append((producer, product))
                         self.producer_dex[producer][i][1] = 0
+                        logging.info('finished adding %s to cart with id %s', product, cart_id)
                         return True
+            logging.error('could not add %s to cart with id %s', product, cart_id)
         return False
 
     def remove_from_cart(self, cart_id, product):
@@ -99,8 +133,12 @@ class Marketplace:
 
         :type product: Product
         :param product: the product to remove from cart
+
+        We remove the product from the cart and change the value of the product on the
+        shelf from 0 to 1
         """
         with self.lock_c:
+            logging.info('try removing %s from cart with id %s', product, cart_id)
             for pair in self.cart_dex[cart_id]:
                 if pair[1] == product:
                     producer_id = pair[0]
@@ -112,6 +150,7 @@ class Marketplace:
                             self.producer_dex[producer_id][i][1] = 1
                             break
                     break
+            logging.info('finished removing %s from cart with id %s', product, cart_id)
 
     def place_order(self, cart_id):
         """
@@ -119,8 +158,13 @@ class Marketplace:
 
         :type cart_id: Int
         :param cart_id: id cart
+
+        we return a list of the items in the cart
+        we remove all the items in the cart and all the items on the shelf
+        that have the value of 0
         """
         with self.lock_c:
+            logging.info('placing order for cart id %s', cart_id)
             order = []
             for pair in self.cart_dex[cart_id]:
                 order.append(pair[1])
@@ -129,4 +173,5 @@ class Marketplace:
                             self.producer_dex[pair[0]][i][1] == 0:
                         del self.producer_dex[pair[0]][i]
                         break
+            logging.info('finished placing order for cart id %s', cart_id)
         return order
